629d64a26905d137ade2cf11ed8bc2c1ef866286
Jacqueline Chenault <adigitalnative@gmail.com>
2018-09-02 22:39:22 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,35 @@
-# Write your code here!
-
-
-
-
-
-
-
-
-
-
+def game_hash
+  {
+    home: {
+      team_name: "",
+      colors: [],
+      players: {
+        name: {
+          number: ""
+          shoe: ""
+          points: ""
+          rebounds: ""
+          assists: ""
+          steals: ""
+          blocks: ""
+          slam_dunks: ""
+        }
+      }
+    },
+    away: {
+      team_name: "",
+      colors: [],
+      players: {
+        name: {
+          number: ""
+          shoe: ""
+          points: ""
+          rebounds: ""
+          assists: ""
+          steals: ""
+          blocks: ""
+          slam_dunks: ""
+        }
+      }
+  }
+end
